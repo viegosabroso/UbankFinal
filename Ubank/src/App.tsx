827da07d3b836ad7c36c80e94da6c870798ac4ad
viegosabroso <Thommmmmmas@hotmail.com>
2024-09-27 10:42:11 -0500

    File: Scaffolding added

---
+++ Ubank/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+import './App.css'
+
+function App() {
+
+   
+  return (
+    <>
+      <h1>Welcome to Ubank</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Ubank/src/Clients/firebase.ts
@@ -0,0 +1,42 @@
+
+
+
+
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAI_jk6jiCWrwv7cwmd4T_OfLbU-8Z97To",
+  authDomain: "ubank-6f760.firebaseapp.com",
+  projectId: "ubank-6f760",
+  storageBucket: "ubank-6f760.appspot.com",
+  messagingSenderId: "757991047784",
+  appId: "1:757991047784:web:b961f22e7afa898fd5f1dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
