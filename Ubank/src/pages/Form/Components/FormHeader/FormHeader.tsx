57ddb1f0eda86154bb44610b3dd54add106df9d7
viegosabroso <Thommmmmmas@hotmail.com>
2024-09-28 18:12:55 -0500

    Feat: Firebase configuration

--- Ubank/src/pages/Form/Components/FormHeader/FormHeader.tsx
+++ Ubank/src/pages/Form/Components/FormHeader/FormHeader.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
+
 const Header: React.FC = () => {
     return (
     <header>
-        <img src="../../../../assets/form_logo.png" alt="Ubank" />
+        <img src="" alt="Ubank" />
     </header>
     );
 };
