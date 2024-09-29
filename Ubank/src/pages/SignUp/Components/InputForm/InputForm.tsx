970afa91efe6ab0f6acdcab1b2cea2771286b2af
Soleil <solangegarciaguamanga@gmail.com>
2024-09-28 20:29:47 -0500

    Feat: Add information

--- Ubank/src/pages/SignUp/Components/InputForm/InputForm.tsx
+++ Ubank/src/pages/SignUp/Components/InputForm/InputForm.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 interface InputFieldProps {
-    label: string;
-    name: string;
-    type?: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  }
+  label: string;
+  name: string;
+  type?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-  const InputForm: React.FC<InputFieldProps> = ({label, name, type = 'text', value, onChange}) => {
-    return(
-        <>
-        <div className="input-field">
+const InputForm: React.FC<InputFieldProps> = ({ label, name, type = 'text', value, onChange }) => {
+  return (
+    <>
+      <div className="input-field">
         <label htmlFor={name}>{label}</label>
         <input
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        required
-      />
-    </div>
-        </>
-    )
-  }
+          id={name}
+          name={name}
+          type={type}
+          value={value}
+          onChange={onChange}
+          required
+        />
+      </div>
+    </>
+  )
+}
 
 export default InputForm;
--- Ubank/src/pages/SignUp/Components/SignUpForm/SignUpForm.tsx
+++ Ubank/src/pages/SignUp/Components/SignUpForm/SignUpForm.tsx
@@ -4,45 +4,45 @@ import useForm from "../../../../Hooks/UseForm";
 import './SignUpForm.css'
 
 const SignUpForm: React.FC = () => {
-    const { values, handleChange, handleSubmit } = useForm();
-  
-    return (
-      <div className="sign-up-container">
-        <div className="left-panel">
-          <h2>Grow your money with <span className="ubank">UBank!</span> 🚀</h2>
-          <div className="illustration">
-            <img src="" alt="" />
-          </div>
-        </div>
-        <div className="right-panel">
-          <h2>Create <span className="highlight">Account</span></h2>
-          <p>Sign up today and keep full control of your finances in one place.</p>
-          <form onSubmit={handleSubmit}>
-            <InputForm
-              label="Full Name"
-              name="fullName"
-              value={values.fullName}
-              onChange={handleChange}
-            />
-            <InputForm
-              label="Email Address"
-              name="email"
-              value={values.email}
-              onChange={handleChange}
-            />
-            <InputForm
-              label="Password"
-              name="password"
-              type="password"
-              value={values.password}
-              onChange={handleChange}
-            />
-            <button type="submit" className="sign-up-button">Sign Up</button>
-          </form>
-          <p className="login-link">Do you already have an Account? <a href="/login">Log In</a></p>
+  const { values, handleChange, handleSubmit } = useForm();
+
+  return (
+    <div className="sign-up-container">
+      <div className="left-panel">
+        <h2>Grow your money with <span className="ubank">UBank!</span> 🚀</h2>
+        <div className="illustration">
+          <img src="https://firebasestorage.googleapis.com/v0/b/ubank-6f760.appspot.com/o/Images%2FPig.png?alt=media&token=638e9e45-609a-4ca3-8855-4aa84c1483b4" alt="" />
         </div>
       </div>
-    );
-  };
-  
-  export default SignUpForm;
+      <div className="right-panel">
+        <h2>Create <span className="highlight">Account</span></h2>
+        <p>Sign up today and keep full control of your finances in one place.</p>
+        <form onSubmit={handleSubmit}>
+          <InputForm
+            label="Full Name"
+            name="fullName"
+            value={values.fullName}
+            onChange={handleChange}
+          />
+          <InputForm
+            label="Email Address"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <InputForm
+            label="Password"
+            name="password"
+            type="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+          <button type="submit" className="sign-up-button">Sign Up</button>
+        </form>
+        <p className="login-link">Do you already have an Account? <a href="/login">Log In</a></p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
