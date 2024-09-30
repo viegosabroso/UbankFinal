738d263ba3e502b38a88b1144f8054288b5ec1cc
mailubb <mailub2004@gmail.com>
2024-09-29 22:28:51 -0500

    Style: Css improving

--- Ubank/src/pages/Form/Components/Answers/Answers.tsx
+++ Ubank/src/pages/Form/Components/Answers/Answers.tsx
@@ -9,6 +9,9 @@ interface AnswerOptionProps {
 //aqui debe haber un map de esta respuesta para que se muestren todas las opciones desde firebase
 const AnswerOption: React.FC<AnswerOptionProps> = ({ text, iconSrc }) => {
     return (
+        // className={`card ${isSelected ? 'selected' : ''}`}
+        // onClick={handleClick}   Poner esto en el div para que se PINTE VERDE la respuesta CLICKEADA
+
         <div className="answer-option">
         <img src={img} alt="icon" />
         <p>{text}</p>
