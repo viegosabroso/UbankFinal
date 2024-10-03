017480a940723ae28ade3d92e354c0320c2366c4
viegosabroso <Thommmmmmas@hotmail.com>
2024-10-03 15:57:05 -0500

    Fix: Deploy not working

--- Ubank/src/navigation/Navigation.tsx
+++ Ubank/src/navigation/Navigation.tsx
@@ -1,11 +1,11 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import FinantialPlan from "../pages/FinantialPlan/FinantialPlan";
 import { lazy, Suspense } from "react";
 import OnBoarding from "../pages/OnBoarding/OnBoarding";
 const FormPage = lazy(() => import("../pages/Form/Form"));
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
