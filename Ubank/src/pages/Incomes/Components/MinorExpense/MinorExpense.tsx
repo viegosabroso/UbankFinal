cca61141fb08060750a58aed109b2cdc41b0396f
JaAn24 <vargasjaime0224@hotmail.com>
2024-10-20 20:18:11 -0500

    Fix : Add MinorExpense CSS

--- Ubank/src/pages/Incomes/Components/MinorExpense/MinorExpense.tsx
+++ Ubank/src/pages/Incomes/Components/MinorExpense/MinorExpense.tsx
@@ -1,28 +1,26 @@
-interface MinorExpenseProps {
-    Expensetype : string,
-    ExpenseName : string,
-    ExpenseDate : string,
-    ExpenseAmount : number
+import './MinorExpense.css';
 
+interface MinorExpenseProps {
+    Expensetype: string;
+    ExpenseName: string;
+    ExpenseDate: string;
+    ExpenseAmount: number;
 }
 
-const MinorExpense = ({Expensetype, ExpenseName, ExpenseDate, ExpenseAmount} : MinorExpenseProps) => {
-    return(
-        <div>
-            <img src ="" alt = "" />
-            <div>
-        <h2>{Expensetype}</h2>
-        <p>{ExpenseName}</p>
-        <p>{ExpenseDate}</p>
-
-
-        </div>
-        <div>  
-            <h3>{ExpenseAmount}</h3>
+const MinorExpense = ({ Expensetype, ExpenseName, ExpenseDate, ExpenseAmount }: MinorExpenseProps) => {
+    return (
+        <div className="minor-expense">
+            <img src="" alt="Circular icon" />
+            <div className="details">
+                <h2>{Expensetype}</h2>
+                <p>{ExpenseName}</p>
+                <p>{ExpenseDate}</p>
+            </div>
+            <div className="amount">
+                <h3>{ExpenseAmount}</h3>
             </div>
         </div>
-    )
-
-
+    );
 }
+
 export default MinorExpense;
